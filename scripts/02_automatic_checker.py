9d1fe351b9ad4a2574d3142640baf4c4aa4c460f
leo-grz <simpleip@web.de>
2024-11-18 12:03:09 +0100

    reomved contour checker, only darkness check now

--- scripts/02_automatic_checker.py
+++ scripts/02_automatic_checker.py
@@ -23,8 +23,7 @@ PATH_TO_LABELS = PATH_TO_DATA + 'processed/testing_dataset_top20_max50.csv'
 PATH_TO_IMAGES = PATH_TO_DATA + 'processed/testing_dataset_top20_max50_images'
 
 DARKNESS_BLACKLIST_THRESHOLD = 0.05
-DARKNESS_CHECKLIST_THRESHOLD = 0.15
-CONTOUR_CHECKLIST_THRESHOLD = 50
+DARKNESS_CHECKLIST_THRESHOLD = 0.1
 
 STATES_TO_IGNORE= ['CHECK', 'BLACK', 'MISSING']
 
@@ -79,85 +78,14 @@ def calculate_darkness(tensor):
     return average_intensity, is_dark, is_black
 
 
-def estimate_object_size(tensor):
-    """
-    Calculate the size of the biggest object in the image.
-
-    This function processes an image tensor, converts it to grayscale, applies edge detection,
-    and finds the largest contour to estimate the object's size in pixels.
-
-    Parameters: 
-    tensor (torch.Tensor): A tensor of shape (3, H, W) representing an RGB image.
-
-    Returns:
-    tuple:
-        - area (int): Size of the biggest object in pixels (area of the largest contour).
-        - is_small (bool): True if the area is below the size threshold. 
-    """
-    array = tensor.numpy().transpose((1, 2, 0))
-    image = np.clip(array * 255, 0, 255).astype(np.uint8)
-
-    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)  # Convert the image to grayscale
-    edges = cv2.Canny(gray, 100, 200)  # Apply Canny edge detector
-    contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)  # Find contours
-
-    if contours:  # Calculate the area of the largest contour
-        largest_contour = max(contours, key=cv2.contourArea)
-        area = cv2.contourArea(largest_contour)
-        is_small = area < CONTOUR_CHECKLIST_THRESHOLD
-        return area, is_small
-    else:
-        return 0, False
-
-def is_contour_at_edge(contour, img_shape):
-    for point in contour:
-        if point[0][0] == 0 or point[0][1] == 0 or point[0][0] == img_shape[1] - 1 or point[0][1] == img_shape[0] - 1:
-            return True
-    return False
-
-def filter_image(tensor, contour_threshold=100):
-    array = tensor.numpy().transpose((1, 2, 0))
-    image = np.clip(array * 255, 0, 255).astype(np.uint8)
-
-    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)  # Convert the image to grayscale
-    edges = cv2.Canny(gray, 50, 100)  # Apply Canny edge detector with adjusted thresholds
-    contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)  # Find contours
-    valid_contours = [contour for contour in contours if not is_contour_at_edge(contour, image.shape)]
-    valid_contours = [contour for contour in contours if 500 < cv2.contourArea(contour) < 35000]
-
-    # for contour in contours:
-    #     print(int(cv2.contourArea(contour)))
-    #     print(500 < int(cv2.contourArea(contour)) < 40000)
-
-    if valid_contours:
-        largest_contour = max(valid_contours, key=cv2.contourArea)
-        area = cv2.contourArea(largest_contour)
-
-        # Filter based on the number of valid contours and the area of the largest valid contour
-        if len(valid_contours) > 5:
-            print(f"Filtered due to multiple butterflies. Contours: {len(valid_contours)}")
-            return area, True
-        elif area < contour_threshold:
-            print(f"Filtered due to weak contour. Area: {area}")
-            return area, True
-        else:
-            return area, False
-    else:
-        print("Filtered due to no valid contours.")
-        return 0, True
-
-i = 0
-for images, labels, gbifids, img_names in dataloader:
-    i +=1
-    print(f'Bach [{i}/{len(dataloader)}]')
+for batch, (images, labels, gbifids, img_names) in enumerate(dataloader, start=1):
+    print(f'Bach [{batch}/{len(dataloader)}]')
     
     for image, label, gbifid, img_name in zip(images, labels, gbifids, img_names):
         if mode_new:
             print(f'[MODE_NEW] Processing Sample with ID {gbifid} and Filename {img_name}')
 
         average_intensity, is_dark, is_black = calculate_darkness(image)
-        object_size, is_small = estimate_object_size(image)
-        #object_size, is_small = filter_image(image)
 
         if is_black: # if image is without a doubt too dark, automatically add it to blacklist
             blacklist.append(int(gbifid))
